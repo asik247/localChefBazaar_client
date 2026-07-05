0fe9d4c0cf1df04654e7970ff195b895bc3b40ec
Asikur Rahaman <mdasikur5893@gmail.com>
2026-07-06 01:19:54 +0600

    admin panel ManageRequest page done

--- src/Pages/DashBoard/AdminDashboard/ManageRequest.jsx
+++ src/Pages/DashBoard/AdminDashboard/ManageRequest.jsx
@@ -19,6 +19,7 @@ const ManageRequest = () => {
 
     if (isLoading) {
         return <Loading />;
+        
     }
     //! handlerRequstAccepted;
     const handlerRequstAccepted = (id, email, requestType) => {
