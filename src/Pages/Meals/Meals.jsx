ba4bc4af7ce7012d2025cde77afb58989cb98fec
Asikur Rahaman <mdasikur5893@gmail.com>
2026-07-08 22:59:11 +0600

    meals page search implement

--- src/Pages/Meals/Meals.jsx
+++ src/Pages/Meals/Meals.jsx
@@ -12,23 +12,45 @@ const Meals = () => {
             return res.data
         }
     })
+    //! serarch input field code implement;
+    const handlerSearch = (e)=>{
+        console.log(e);
+    }
     return (
         <div>
             <Helmet>
-                 <title>Meals | LocalChefBazaar</title>
+                <title>Meals | LocalChefBazaar</title>
             </Helmet>
             {/* <h1>All Meals here{meals.length}</h1> */}
             <div className="text-center my-10">
                 <h1 className="text-3xl md:text-4xl font-extrabold tracking-tight italic text-base-content">
                     All Freshly Made <span className="text-emerald-500">for You</span>
                 </h1>
-                <p className="mt-3 text-sm text-base-content/60">
+                <p className="mt-3 text-shadow-2xs text-base-content/60">
                     Showing{' '}
                     <span className="badge badge-error badge-outline font-semibold align-middle">
                         {meals.length}
                     </span>{' '}
                     {meals.length === 1 ? 'meal' : 'meals'} available right now
                 </p>
+                {/* Search input field */}
+                <div className='my-4'>
+                    <label className="input">
+                        <svg className="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                            <g
+                                strokeLinejoin="round"
+                                strokeLinecap="round"
+                                strokeWidth="2.5"
+                                fill="none"
+                                stroke="currentColor"
+                            >
+                                <circle cx="11" cy="11" r="8"></circle>
+                                <path d="m21 21-4.3-4.3"></path>
+                            </g>
+                        </svg>
+                        <input onChange={(e)=>handlerSearch(e.target.value)} type="search" required placeholder="Search" />
+                    </label>
+                </div>
             </div>
             <div className='mt-20'>
                 <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8'>
